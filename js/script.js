bb0265d38ca34038a1f66be37ec4466102336d3a
Jose Guerra <jose@gcostudios.com>
2018-06-18 07:18:37 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+"use strict";
+	
+	$('.slider').flickity({
+		cellAlign: 'left',
+		pageDots: false,
+		wrapAround: true,
+		selectedAttraction: 0.01,
+		friction: 0.15,
+		autoPlay: false
+	});
+	
+});
